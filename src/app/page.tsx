7175e213e20204cfd9d6c525f5b9438380e5054a
Programeitor <f.j.gg2@hotmail.com>
2026-01-24 13:45:20 +0100

    chore: configure commit hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
       <h1 className="text-4xl font-bold text-blue-600">
-        Portfolio Next.js + Tailwind listo 🚀 y vamos a probar con 
-        Husky
+        Portfolio Next.js + Tailwind listo 🚀 
       </h1>
+      <h2 className="text-2xl"> Bien, vamos a probar con 
+        Husky y ahora podemos poner todo lo demás </h2>
     </main>
   );
 }
